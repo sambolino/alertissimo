0c8de7b322fefce4afc4d0510a29819c5d3f4dcf
Veljko Vujcic <sambolino@gmail.com>
2025-07-15 14:54:44 +0200

    added safe api token loading

---
+++ alertissimo/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+# Load values from .env into environment
+load_dotenv()
+
+# Access tokens and credentials
+LASAIR_TOKEN = os.getenv("LASAIR_TOKEN")
+FINK_USER = os.getenv("FINK_USER")
+FINK_PASS = os.getenv("FINK_PASS")
