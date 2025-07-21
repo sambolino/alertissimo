cd70527acefb6793a98ed1a2d016de7bd2a7ea40
Veljko Vujcic <sambolino@gmail.com>
2025-07-21 22:25:05 +0200

    changed brackets

--- alertissimo/config.py
+++ alertissimo/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # Access tokens and credentials
 LASAIR_TOKEN = st.secrets["LASAIR_TOKEN"]
-FINK_USERNAME = st.secrets("FINK_USERNAME")
-FINK_GROUP_ID = st.secrets("FINK_GROUP_ID")
+FINK_USERNAME = st.secrets["FINK_USERNAME"]
+FINK_GROUP_ID = st.secrets["FINK_GROUP_ID"]
 
 DEFAULT_TIMEOUT = 30  # seconds
