9b7a6877b71be1690c1d28bfee003393bb2df9d0
Veljko Vujcic <sambolino@gmail.com>
2026-08-10 12:19:52 +0200

    Add internal portfolio representation

---
+++ alertissimo/core/portfolio/__init__.py
@@ -0,0 +1,27 @@
+"""Internal portfolio models for concrete semantic records and connections."""
+
+from .models import (
+    InternalEdgeId,
+    InternalExecutionId,
+    InternalExecutionProvenance,
+    InternalPortfolioId,
+    InternalRecordId,
+    InternalRecordSource,
+    Portfolio,
+    PortfolioModelError,
+    SemanticEdge,
+    SemanticRecord,
+)
+
+__all__ = (
+    "InternalEdgeId",
+    "InternalExecutionId",
+    "InternalExecutionProvenance",
+    "InternalPortfolioId",
+    "InternalRecordId",
+    "InternalRecordSource",
+    "Portfolio",
+    "PortfolioModelError",
+    "SemanticEdge",
+    "SemanticRecord",
+)
