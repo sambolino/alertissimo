069ebf19433860175204584666678a97a071196d
Veljko Vujcic <sambolino@gmail.com>
2026-08-20 15:11:01 +0200

    Export semantic predicate IR

--- alertissimo/orchestration/ir/__init__.py
+++ alertissimo/orchestration/ir/__init__.py
@@ -10,16 +10,30 @@ from .models import (
     SearchStep, SemanticSearchStep, Source, SqlQueryStep, Step, StepUnion,
     TargetKind, TargetSelector, TimeContext, UtilityScoreStep, WorkflowIR,
 )
+from .predicates import (
+    BooleanPredicate,
+    ComparisonPredicate,
+    ExistsPredicate,
+    NotPredicate,
+    Predicate,
+    PredicateLiteral,
+    PredicateModel,
+    SemanticReference,
+    and_predicates,
+    iter_semantic_references,
+)
 
 __all__ = [
-    "ActionStep", "AggregateStep", "AnalyzeStep", "ClassifyStep", "ColorColorStep",
-    "ColorMagnitudeStep", "CompareStep", "ConeSearchStep", "ConfirmStep",
-    "DeriveStep", "ExportStep", "FilterStep", "FollowupRequestStep",
+    "ActionStep", "AggregateStep", "AnalyzeStep", "BooleanPredicate",
+    "ClassifyStep", "ColorColorStep", "ColorMagnitudeStep", "CompareStep",
+    "ComparisonPredicate", "ConeSearchStep", "ConfirmStep", "DeriveStep",
+    "ExistsPredicate", "ExportStep", "FilterStep", "FollowupRequestStep",
     "GetClassificationStep", "GetCrossmatchStep", "GetCutoutStep",
     "GetDataProductStep", "GetForcedPhotometryStep", "GetLightcurveStep",
     "GetSpectrumStep", "GetStep", "LightcurveStep", "LookupStep", "MatchStep",
-    "MethodAnalysisStep", "MonitorStep", "NotifyStep", "SearchSelection",
-    "SearchStep", "SemanticSearchStep", "Source", "SqlQueryStep", "Step",
+    "MethodAnalysisStep", "MonitorStep", "NotPredicate", "NotifyStep", "Predicate",
+    "PredicateLiteral", "PredicateModel", "SearchSelection", "SearchStep",
+    "SemanticReference", "SemanticSearchStep", "Source", "SqlQueryStep", "Step",
     "StepUnion", "TargetKind", "TargetSelector", "TimeContext",
-    "UtilityScoreStep", "WorkflowIR",
+    "UtilityScoreStep", "WorkflowIR", "and_predicates", "iter_semantic_references",
 ]
